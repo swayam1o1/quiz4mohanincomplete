33af89154b43a64aa4446115f18b5cad2990685d
Swayamprabha <swayam.narayanan2005@gmail.com>
2025-06-25 22:15:47 +0530

    Update quizRoutes.js

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const quizController = require('../controllers/quizController');
-
 const verifyToken = require('../middleware/verifyToken');
 
+// Admin routes (require token)
 router.post('/create', verifyToken, quizController.createQuiz);
 router.post('/:quizId/questions', verifyToken, quizController.addQuestion);
 router.put('/:quizId/questions/:questionId', verifyToken, quizController.updateQuestion);
 router.delete('/:quizId/questions/:questionId', verifyToken, quizController.deleteQuestion);
-router.get('/:quizId/questions', verifyToken, quizController.getQuestionsByQuiz);
-router.post('/:quizId/start', verifyToken, quizController.startQuiz);
-router.get('/:quizId/state', quizController.getQuizState);
-router.get('/validate/:code', quizController.validateQuizCode);
-router.post('/:code/questions/:questionId/answer', quizController.submitAnswer);
-router.get('/:code/leaderboard', quizController.getLeaderboard);
 router.get('/list', verifyToken, quizController.listQuizzes);
+router.get('/:quizId/questions', verifyToken, quizController.getQuizQuestions);
+router.get('/start/:quizId', verifyToken, quizController.startQuiz);
+
+// User-facing routes (no token required)
+router.get('/validate/:code', quizController.validateQuizCode);
+router.post('/submit', quizController.submitQuiz);
+router.get('/:code/leaderboard', quizController.getLeaderboard);
+router.get('/:quizId/state', quizController.getQuizState);
 
 module.exports = router;
